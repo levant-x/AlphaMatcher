2923b3023a400f8004b2ab00df3ad7bb2a56357d
levant-x <62507685+levant-x@users.noreply.github.com>
2021-08-21 19:10:29 +0500

    initial draft setup

---
+++ admin-tours-all.php
@@ -0,0 +1,33 @@
+<section class="admin-header">
+  <h2 class="admin-header--title">
+    Туры
+  </h2>
+  <div class="admin-header--controls">
+    <div class="tabs">
+      <a href="./domains/admin/admin-tours-all.php">Предстоящие</a>
+      <a href="./domains/admin/admin-tours-compl.php">Прошедшие</a>
+    </div>
+    <div class="add-new">
+
+    </div>
+  </div>
+</section>
+
+<section class="admin-content">
+  <h2>
+    Здесь скоро будет список всех туров
+  </h2>
+
+  <div>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia odio magni placeat, unde doloremque earum, obcaecati dolorum officia ullam deserunt culpa fuga, praesentium vero animi quo provident nobis ad dolorem!
+    Ea, dolor quod tempora illum perferendis similique dignissimos enim praesentium eveniet ipsum nobis assumenda doloribus recusandae minima tenetur, accusamus consectetur voluptate cumque impedit ipsa quisquam. Fugiat, ad. Recusandae, mollitia illo.
+    Iure, ut? Quas voluptatum cumque ullam ut temporibus itaque consequatur, commodi beatae impedit quibusdam velit quaerat, fugit odio dolore asperiores? Porro velit, magnam quam possimus illum placeat aspernatur quisquam nostrum?
+    Omnis velit at corporis quisquam accusantium! Voluptatum suscipit obcaecati culpa, adipisci numquam doloremque. Quia, velit sed repellendus est temporibus vero ipsa ad molestiae voluptate id officia cumque earum atque fugiat?
+    Officia expedita nesciunt, commodi molestias dolorem possimus aliquid dolores accusamus consequuntur cum quas ad dignissimos iusto voluptatem ex laboriosam. Corrupti, corporis unde! Quae, eaque est quod magni architecto corrupti commodi.
+    Nisi, laudantium! Vel libero quae inventore facere sequi nihil unde a, veritatis deleniti dolorem nemo, et dolor ipsam facilis, molestias laudantium voluptatem mollitia! Enim id consequuntur quis! Numquam, totam voluptatem.
+    Earum voluptas nesciunt repellendus molestias possimus voluptatem architecto obcaecati vitae doloribus consectetur, ipsa inventore, repellat quasi dolorum cumque nulla quisquam aut! Dicta maxime sequi sit nobis delectus deleniti ratione illo.
+    Iste exercitationem, est distinctio fugit dignissimos quia sit ea rerum quod mollitia, repellendus fuga consequuntur? Tempore illo inventore quaerat! Officiis maiores ea harum neque dolore doloribus temporibus assumenda rerum minus!
+    Excepturi, eos dignissimos. Repellendus, consequatur nobis minima quasi voluptate magni sed fugiat libero itaque dolore qui autem nisi voluptates corrupti perspiciatis labore necessitatibus aliquid ducimus dolorem quo debitis architecto? Veritatis!
+    Illum doloremque optio natus eum corrupti! Voluptatibus exercitationem eum ducimus quo amet nihil, eligendi velit laudantium debitis recusandae totam mollitia odio culpa! Architecto, possimus enim illum ea aliquid impedit beatae!
+  </div>
+</section>
